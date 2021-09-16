c737144353e512267d842997c5c908fb823e70e1
Julio Moreno <Julio23m.y@gmail.com>
2021-09-16 16:15:18 -0400

    Begin task 1

--- config/settings/local.py
+++ config/settings/local.py
@@ -3,3 +3,5 @@ from .base import *
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 CACHE_TIMEOUT = 30
+
+INSTALLED_APPS += ['poke_base_test.pokemon', ]
---
+++ poke_base_test/pokemon/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PokemonConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'poke_base_test.pokemon'
