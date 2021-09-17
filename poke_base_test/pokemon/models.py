55edfd6e0d8f2b9ec641995cf95940ea6d7fbf38
Julio Moreno <Julio23m.y@gmail.com>
2021-09-17 12:57:10 -0400

    Forgot Capitalice class

--- poke_base_test/pokemon/models.py
+++ poke_base_test/pokemon/models.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 
 # Create your models here.
 
-class TimeStampedModel(models.Model):
+class NameTimeStampedModel(models.Model):
         created = models.DateTimeField(auto_now_add=True)
         modified = models.DateTimeField(auto_now=True)
         name = models.CharField('Name', max_length=20)
@@ -15,12 +15,12 @@ class TimeStampedModel(models.Model):
 
 
 # POKEMON BASE
-class pokemon(TimeStampedModel):
+class Pokemon(NameTimeStampedModel):
     
     id = models.BigAutoField(primary_key=True, unique=True)
     height = models.DecimalField('Height', max_digits=5, decimal_places=2)
     Weight = models.DecimalField('Weight', max_digits=5, decimal_places=2)
-    evolution = models.ManyToManyField('self', null=True,blank=True)
+    evolution = models.ManyToManyField('self',blank=True)
 
     class Meta:
         verbose_name = ("pokemon")
@@ -34,7 +34,7 @@ class pokemon(TimeStampedModel):
 
 
 # POKEMON MOVE
-class move(TimeStampedModel):
+class Move(NameTimeStampedModel):
     
     
     class Meta:
@@ -48,7 +48,7 @@ class move(TimeStampedModel):
         return reverse("move_detail", kwargs={"pk": self.pk})
 
 # POKEMON ABILITY
-class ability(TimeStampedModel):
+class Ability(NameTimeStampedModel):
     
     
 
@@ -63,7 +63,7 @@ class ability(TimeStampedModel):
         return reverse("ability_detail", kwargs={"pk": self.pk})
 
 # POKEMON FORM
-class form(TimeStampedModel):
+class Form(NameTimeStampedModel):
     
     
 
@@ -79,7 +79,7 @@ class form(TimeStampedModel):
 
 
 # POKEMON FORM
-class type(TimeStampedModel):
+class Type(NameTimeStampedModel):
     
     
 
@@ -94,7 +94,7 @@ class type(TimeStampedModel):
         return reverse("type_detail", kwargs={"pk": self.pk})
 
 # POKEMON GAME INDICE
-class game_indice(TimeStampedModel):
+class GameIndice(NameTimeStampedModel):
     
     
     
