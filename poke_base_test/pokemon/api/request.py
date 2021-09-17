b008c06cedd941cb55e0be42f2bddaf7352d95c3
Julio Moreno <Julio23m.y@gmail.com>
2021-09-17 08:40:28 -0400

    Adding error exceptions

--- poke_base_test/pokemon/api/request.py
+++ poke_base_test/pokemon/api/request.py
@@ -1,4 +1,4 @@
-import django
+from os import error
 import requests
 
 
@@ -7,60 +7,68 @@ URL_BASE_POKEMON = 'https://pokeapi.co/api/v2/'
 
 def pokemon_info(self,id):
     dic_pokemon = dict()
-    r = requests.get(URL_BASE_POKEMON+f'pokemon/{id}')
-    if r.ok:
-        request_information = r.json()
-        dic_pokemon['id'] = request_information['id']
-        dic_pokemon['name'] = request_information['name'].capitalize()
-        dic_pokemon['height'] =request_information['height']
-        dic_pokemon['weight'] = request_information['weight']
-        dic_pokemon['stats'] = stats(self,request_information['stats'])
-        
-        dic_pokemon['evolution_chain'] = evolution_chain(self,id)
-        return dic_pokemon
+    try:
+        r = requests.get(URL_BASE_POKEMON+f'pokemon/{id}')
+        if r.ok:
+            request_information = r.json()
+            dic_pokemon['id'] = request_information['id']
+            dic_pokemon['name'] = request_information['name'].capitalize()
+            dic_pokemon['height'] =request_information['height']
+            dic_pokemon['weight'] = request_information['weight']
+            dic_pokemon['stats'] = stats(request_information['stats'])
+            dic_pokemon['evolution_chain'] = evolution_chain(id)
+            if not type(dic_pokemon['evolution_chain']) == list:
+                return dic_pokemon['evolution_chain']
+                
+            return dic_pokemon
+        else:
+            return f"code {r.reason}, {r.status_code}" 
+    except error:
+
+        return "Fetch api pokemon information"
 
 
-def evolution_chain(self,id):
-    r = requests.get(URL_BASE_POKEMON+f'pokemon-species/{id}')
-    ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n//10%10!=1)*(n%10<4)*n%10::4])
-    list_ordinal = [ordinal(n) for n in range(1,32)]
-    list_pokemon = []
-    if r.ok:
-        dic_json = r.json()
-        dic_response = dict()
-        dic_evolution = dict(requests.get(dic_json['evolution_chain']['url']).json())
-        dic_response[list_ordinal[0]] = dic_evolution['chain']['species']['name'].capitalize()
-        list_pokemon.append(dic_response)
-        for evolution in dic_evolution['chain']['evolves_to']:
-            dic_response=dict()
-            if len(dic_evolution['chain']['evolves_to'])>1:
-                dic_response['alternative'] = True
-            else:
-                dic_response['alternative'] = False
-            dic_response[list_ordinal[1]] = evolution['species']['name'].capitalize()
+
+def evolution_chain(id):
+    try:
+        r = requests.get(URL_BASE_POKEMON+'pokemon')
+        r = requests.get(URL_BASE_POKEMON+f'pokemon-species/{id}')
+        ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n//10%10!=1)*(n%10<4)*n%10::4])
+        list_ordinal = [ordinal(n) for n in range(1,32)]
+        list_pokemon = []
+        if r.ok:
+            dic_json = r.json()
+            dic_response = dict()
+            dic_evolution = dict(requests.get(dic_json['evolution_chain']['url']).json())
+            dic_response[list_ordinal[0]] = dic_evolution['chain']['species']['name'].capitalize()
             list_pokemon.append(dic_response)
-            for evolution_2 in evolution['evolves_to']:
-                dic_response = dict()
-                if len(evolution_2['evolves_to'])>1:
+            for evolution in dic_evolution['chain']['evolves_to']:
+                dic_response=dict()
+                if len(dic_evolution['chain']['evolves_to'])>1:
                     dic_response['alternative'] = True
                 else:
                     dic_response['alternative'] = False
-
-                dic_response[list_ordinal[2]] =evolution_2['species']['name'].capitalize()
+                dic_response[list_ordinal[1]] = evolution['species']['name'].capitalize()
                 list_pokemon.append(dic_response)
-        return list_pokemon        
-                
+                for evolution_2 in evolution['evolves_to']:
+                    dic_response = dict()
+                    if len(evolution_2['evolves_to'])>1:
+                        dic_response['alternative'] = True
+                    else:
+                        dic_response['alternative'] = False
+
+                    dic_response[list_ordinal[2]] =evolution_2['species']['name'].capitalize()
+                    list_pokemon.append(dic_response)
+            return list_pokemon
+        else:
+            return f"code {r.reason}, {r.status_code}"
+    except error:
+        return "Fetch api evolution"
 
 
 
-            
-        
 
-        
-        return dic_json
-
-
-def stats(self,stats):
+def stats(stats):
     list_stats = []
     if len(stats) > 0:
         for stat in stats:
--- poke_base_test/pokemon/management/commands/pokemonid.py
+++ poke_base_test/pokemon/management/commands/pokemonid.py
@@ -14,20 +14,23 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         
         pokemon = pokemon_info(self,options['pokemonid'])
-        self.stdout.write(self.style.WARNING('Information'))
-        for poke in pokemon:
-            if type(pokemon[poke]) == list and len(pokemon[poke]) > 0:
-                second_level = pokemon[poke]
-                self.stdout.write(self.style.WARNING(poke.capitalize().replace("_"," ")))
-                for i,data in enumerate(second_level):
-                    for key2,value in data.items():
-                        if key2 == 'alternative':
-                            if value ==True:
-                                self.stdout.write(self.style.ERROR(key2.capitalize()))
+
+        if type(pokemon) == dict:
+            self.stdout.write(self.style.WARNING('Information'))
+            for poke in pokemon:
+                if type(pokemon[poke]) == list and len(pokemon[poke]) > 0:
+                    second_level = pokemon[poke]
+                    self.stdout.write(self.style.WARNING(poke.capitalize().replace("_"," ")))
+                    for i,data in enumerate(second_level):
+                        for key2,value in data.items():
+                            if key2 == 'alternative':
+                                if value ==True:
+                                    self.stdout.write(self.style.ERROR(key2.capitalize()))
+                                else:
+                                    continue
                             else:
-                                continue
-                        else:
-                            self.stdout.write(self.style.SUCCESS(key2.capitalize().replace("_"," ")) + ": " +str(value))
-            else:
-                self.stdout.write(self.style.SUCCESS(poke.capitalize().replace("_"," "))+": "+ str(pokemon[poke]))
-            
+                                self.stdout.write(self.style.SUCCESS(key2.capitalize().replace("_"," ")) + ": " +str(value))
+                else:
+                    self.stdout.write(self.style.SUCCESS(poke.capitalize().replace("_"," "))+": "+ str(pokemon[poke]))
+        else:
+            self.stdout.write(self.style.ERROR("Error in consulting api: "+ str(pokemon)))
